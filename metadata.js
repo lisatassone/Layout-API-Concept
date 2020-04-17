146491776cba71d2142bcd4fec3346499a2916f5
Revel Roestorf <rroestorf@skedulo.com>
2020-04-17 14:17:27 +1000

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -1,159 +1,117 @@
 // From layout API:
 
-const layout = {
-  Crud: {
-    Ambulances: {
-      schema: "Ambulances",
-      mapping: "__ambulances",
-      defaultSchemaLabel: "Ambulances",
-      defaultField: "rego",
-      views: {
-        create: {
-          version: "1",
-          layout: { cols: 1 },
+{
+  schemaName: "Ambulances",
+  mapping: "__ambulances",
+  schemaLabel: "Ambulances",
+  primaryField: "rego",
+  formValidations: [
+    {
+      subjectFieldId: '1234', 
+      operation: "=",
+      referenceFieldId: '5678'
+    }
+  ],
+  layouts: {
+    create: {
+      version: "1",
+      columns: [
+        {
           sections: [
             {
-              heading: null,
+              heading: '',
               fields: [
                 {
                   id: "345-3245-3245",
                   name: "Service Interval",
                   type: "picklist",
                   defaultValue: { id: 1, name: "Bi-annually" },
-                  required: true,
+                  visible: true,
                   readOnly: false,
-                  hidden: false,
                   values: [
                     { id: "1", name: "Bi-annually" },
                     { id: "2", name: "Annally" }
-                  ]
+                  ],
+                  // Used for all field level validations / constraints
+                  fieldValidations: {
+                    required: true,
+                  }
                 },
                 {
                   id: "12-23-34-45",
                   name: "Paramedic",
+                  mapping: "paramedicId",
                   type: "reference",
                   defaultValue: null,
+                  visible: true,
                   readOnly: false,
-                  required: false,
-                  hidden: false,
                   // any other field level definitions that make sense, maxLength for text fields? Or should that be a constraint?
-                  metadata: {
-                    schema: "Resources",
+                  referenceSchema: {
+                    name: "Resources",
                     mapping: "__resource",
-                    fieldMapping: "paramedicId"
                   },
-                  constraints: [
-                    // more complex field validation and cross validations
-                    [
-                      // each array is 'and' conditions, separated by 'or' condition
-                      {
-                        operation: "regex",
-                        value: "/[34]/"
-                        // error message context?
-                      },
-                      {
-                        operation: "=",
-                        value: "34"
-                      },
-                      {
-                        operation: "contains",
-                        value: "34"
-                      }
-                    ],
-                    [
-                      {
-                        operation: ">=",
-                        id: "id of another field" // use the value of this field for comparison
-                      }
-                    ]
-                  ],
-                  conditions: [
-                    // run validation when these things happen
-                    {
-                      operation: "!==",
-                      id: "345-3245-3245",
-                      value: null
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        update: {
-          version: "1",
-          layout: { cols: 2 },
-          sections: [
-            {
-              heading: null,
-              fields: [
-                {
-                  id: "12-23-34-45",
-                  name: "Paramedic",
-                  type: "reference",
-                  defaultValue: null,
-                  readOnly: false,
-                  required: false,
-                  hidden: false,
-                  // any other field level definitions that make sense, maxLength for text fields? Or should that be a constraint?
-                  metadata: {
-                    schema: "Resources",
-                    mapping: "__resource",
-                    fieldMapping: "paramedicId"
-                  },
-                  constraints: [
-                    [
-                      // each array is 'and' conditions, separated by 'or' condition
-                      {
-                        operation: "regex",
-                        value: "/[34]/"
-                      },
-                      {
-                        operation: "=",
-                        value: "34"
-                      }
-                    ],
-                    [
-                      {
-                        operation: ">=",
-                        id: "id of another field" // use the value of this field for comparison
-                      }
-                    ]
-                  ],
-                  conditions: [
-                    // run validation when these things happen
-                    {
-                      operation: "!==",
-                      id: "345-3245-3245",
-                      value: null
-                    }
-                  ]
-                }
-              ]
-            },
-            {
-              heading: null,
-              fields: [
-                {
-                  id: "345-3245-3245",
-                  name: "Service Interval",
-                  type: "picklist",
-                  defaultValue: { id: 1, name: "Bi-annually" },
-                  required: true,
-                  readOnly: false,
-                  hidden: false,
-                  values: [
-                    { id: "1", name: "Bi-annually" },
-                    { id: "2", name: "Annally" }
-                  ]
+                  fieldValidations: {
+                    required: true,
+                    contains: "34"
+                  }
                 }
               ]
             }
           ]
         }
+      ]
+      update: {
+        version: "1",
+        columns: [
+          {
+            sections: [
+              {
+                heading: null,
+                fields: [
+                  {
+                    id: "12-23-34-45",
+                    name: "Paramedic",
+                    mapping: "paramedicId",
+                    type: "reference",
+                    defaultValue: null,
+                    visible: true,
+                    readOnly: false,
+                    // any other field level definitions that make sense, maxLength for text fields? Or should that be a constraint?
+                    referenceSchema: {
+                      name: "Resources",
+                      mapping: "__resource",
+                    },
+                    fieldValidations: {
+                      required: true,
+                      contains: "34"
+                    }
+
+                  }
+                ]
+              },
+              {
+                heading: null,
+                fields: [
+                  {
+                    id: "345-3245-3245",
+                    name: "Service Interval",
+                    type: "picklist",
+                    defaultValue: { id: 1, name: "Bi-annually" },
+                    visible: true,
+                    readOnly: false,
+                    values: [
+                      { id: "1", name: "Bi-annually" },
+                      { id: "2", name: "Annally" }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+       ]
       }
     }
-  }
+  ]
 };
 
 // Create/update layout payload
